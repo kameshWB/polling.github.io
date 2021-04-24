64d20ac5d1eb4a6ee55aa12f5b9e047787f382bd
Kamesh Adadadi <58357944+kameshWB@users.noreply.github.com>
2021-04-25 00:08:25 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function onLoginPressed(){
+  document.getElementById('home').style.visibility = 'hidden';
+  document.getElementById('loginAction').style.visibility = 'visible';
+}
+
+function onSignupPressed(){
+  document.getElementById('home').style.visibility = 'hidden';
+  document.getElementById('signupAction').style.visibility = 'visible';
+}
+
+function loginUser(){}
+function signupUser(){}
